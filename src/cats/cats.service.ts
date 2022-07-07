ca2cc770b710ee8433120fc5712ec52e4569932e
zhanghaojie <1455123087@qq.com>
2022-07-07 12:49:49 +0800

    feat: "Hello World" & cats CRUD (with mongoose)

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat, CatDocument } from './schemas/cat.schema';
+
+@Injectable()
+export class CatsService {
+  constructor(
+    @InjectModel(Cat.name) private readonly catModel: Model<CatDocument>,
+  ) {}
+
+  async create(createCatDto: CreateCatDto) {
+    return await this.catModel.create(createCatDto);
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Cat> {
+    return this.catModel.findOne({ _id: id }).exec();
+  }
+
+  async delete(id: string) {
+    return await this.catModel.findByIdAndRemove({ _id: id }).exec();
+  }
+}
